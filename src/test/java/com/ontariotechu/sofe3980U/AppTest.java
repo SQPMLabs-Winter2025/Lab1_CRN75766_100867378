3dcd899138490379f6d234f6e6f35140450cb24f
SQPMLabs1008 <tahnia.haq@ontariotechu.net>
2025-01-15 03:40:09 -0500

    Files from src folder in BinaryCalculator
    Includes WHOLE project folder (except target folder)

---
+++ src/test/java/com/ontariotechu/sofe3980U/AppTest.java
@@ -0,0 +1,22 @@
+package com.ontariotechu.sofe3980U;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/** Name: Nuha Tahnia Haq
+ *  Student ID: 100867378 */
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
